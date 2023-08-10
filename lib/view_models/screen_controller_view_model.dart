690ff06ee42beef579c9a6b8338b7680961ea9a8
GregPeroutka_MacbookAir <ga4peroutka@gmail.com>
2023-08-09 20:51:31 -0500

    Updated login UI and am tying viewmodel - Not done

---
+++ lib/view_models/screen_controller_view_model.dart
@@ -0,0 +1,36 @@
+// Handles main initialization of the app - Connecting to DB, connecting to Account, etc
+import 'dart:async';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import '../firebase_options.dart';
+
+
+class ScreenControllerViewModel {
+
+
+  static final ScreenControllerViewModel _instance = ScreenControllerViewModel._internal();
+  factory ScreenControllerViewModel() {
+    return _instance;
+  }
+  ScreenControllerViewModel._internal();
+
+  StreamController<String> screenStreamController = StreamController<String>();
+  
+  Future<bool> initApp() async {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+
+    FirebaseAuth.instance.authStateChanges().listen((User? user) {
+      if(user == null) {
+        print('User is NOT logged in');
+        screenStreamController.sink.add('loginScreen');
+      } else {
+        print('User IS logged in');
+        screenStreamController.sink.add('mainScreen');
+      }
+    });
+    
+    return true;
+  }
+}

225e5b07b1afcda6f08bf54844d3abb29dcf5053
GregPeroutka_MacbookAir <ga4peroutka@gmail.com>
2023-08-16 17:29:24 -0500

    Replaced Theme setup with Color_Palette implementation

--- lib/main.dart
+++ lib/main.dart
@@ -11,25 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: _lightTheme,
       home: ScreenControllerView(),
     );
   }
 }
-
-ThemeData _lightTheme = ThemeData(
-  colorScheme: const ColorScheme(
-    brightness: Brightness.light,
-    primary: Color.fromARGB(255, 106, 107, 131),
-    onPrimary: Colors.black,
-    secondary: Color.fromARGB(255, 118, 148, 159), 
-    onSecondary: Colors.black, 
-    background: Color.fromARGB(255, 247, 243, 227), 
-    onBackground: Colors.black,
-    error: Colors.red,
-    onError: Colors.black,
-    surface: Color.fromARGB(255, 95, 80, 107),
-    onSurface: Colors.black,
-    primaryContainer: Colors.white
-  )
-);

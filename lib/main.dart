aea6bd4307c33d6f12a84cbdfb7c6faf27474ac4
GregPeroutka_MacbookAir <ga4peroutka@gmail.com>
2023-08-28 17:02:01 -0500

    Progress on Weight logging Page

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(fontFamily: 'Rubik'),
-      home: ScreenControllerView(),
+      home: Scaffold(
+        body: ScreenControllerView()
+      ),
     );
   }
 }
--- lib/views/pages/weight_page.dart
+++ lib/views/pages/weight_page.dart
@@ -1,8 +1,6 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:my_health_journal/color_palette.dart';
-import 'package:my_health_journal/models/database_model.dart';
 import 'package:my_health_journal/types/database_types.dart';
 import 'package:my_health_journal/view_models/weight_page_view_model.dart';
 
@@ -20,11 +18,14 @@ class _WeightPageState extends State<WeightPage> {
   final double _borderRadius = 20;
 
   final int historyCount = 7;
+
   List<WeightData?> historyWeightData = List.empty();
+  WeightData? todaysWeightData;
 
   @override
   Widget build(BuildContext context) {
     historyWeightData = WeightPageViewModel.getLastNDaysWeightData(historyCount);
+    todaysWeightData = WeightPageViewModel.getTodaysWeightData();
 
     return SafeArea(
       bottom: false,
@@ -60,62 +61,83 @@ class _WeightPageState extends State<WeightPage> {
                 ),
               ),
     
-              Center(
-                child: Text(
-                  '181.4 lbs',
-                  style: TextStyle(
-                    color: ColorPalette.currentColorPalette.text,
-                    fontSize: 50,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: "Rubik",
+              Visibility(
+                visible: todaysWeightData != null,
+                child: Center(
+                  child: Text(
+                    todaysWeightData == null
+                      ? '---'
+                      : '${todaysWeightData?.weight} lbs',
+                    style: TextStyle(
+                      color: ColorPalette.currentColorPalette.text,
+                      fontSize: 50,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: "Rubik",
+                    ),
                   ),
                 ),
               ),
-    
+
+              const Padding(padding: EdgeInsets.only(top: 8)),
+
+              Visibility(
+                visible: todaysWeightData == null,
+                child: Container(
+                  clipBehavior: Clip.hardEdge,
+                  decoration: BoxDecoration(
+                    color: ColorPalette.currentColorPalette.primary,
+                    borderRadius: BorderRadius.circular(_borderRadius)
+                  ),
+                  child: TextButton(
+                    onPressed: () => {
+
+
+
+                      // :D
+                      showDialog(
+                        context: context, 
+                        builder: (context) {
+                          return Container(
+                            margin: const EdgeInsets.fromLTRB(
+                              40,
+                              40,
+                              40,
+                              120
+                            ),
+                            decoration: BoxDecoration(
+                              color: ColorPalette.currentColorPalette.primaryBackground,
+                              borderRadius: BorderRadius.circular(_borderRadius),
+                            )
+                          );
+                        }
+                      )
+
+
+
+
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        'Add Weight',
+                        style: TextStyle(
+                          color: ColorPalette.currentColorPalette.text,
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  )
+                ),
+              ),
+
               Container(
                 height: 200,
                 margin: const EdgeInsets.all(8.0),
-                    
                 decoration: BoxDecoration(
                   color: ColorPalette.currentColorPalette.primaryBackground,
                   borderRadius: BorderRadius.circular(_borderRadius),
                 )
-    
-              ),
-    
-              Align(
-                alignment: Alignment.centerRight,
-                child: Padding(
-                  padding: const EdgeInsets.only(right: 8.0),
-                  child: SizedBox(
-                    height: 60,
-                    width: 60,
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: ColorPalette.currentColorPalette.primaryBackground,
-                        shape: BoxShape.circle
-                      ),
-                      child: IconButton(
-                        onPressed: () {
-    
-                          DatabaseModel.writeWeightData(
-                            WeightData(
-                              note: '',
-                              weight: 182.0,
-                              timestamp: Timestamp.fromDate(DateTime.now().subtract(Duration(days: 5)))
-                            )
-                          );
-                          
-                        },
-                        icon: Icon(
-                          Icons.add_rounded,
-                          color: ColorPalette.currentColorPalette.text,
-                          size: 45,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
               ),
     
               const Padding(padding: EdgeInsets.only(top: 8)),
@@ -130,7 +152,6 @@ class _WeightPageState extends State<WeightPage> {
                       color: ColorPalette.currentColorPalette.hintText,
                       fontSize: 30,
                       fontWeight: FontWeight.bold,
-                      fontFamily: "Rubik",
                     ),
                   ),
                 ),
@@ -184,7 +205,6 @@ class _WeightPageState extends State<WeightPage> {
                       style: TextStyle(
                         color: ColorPalette.currentColorPalette.text,
                         fontSize: 24,
-                        fontFamily: "Rubik",
                       ),
                     ),
                       
@@ -195,7 +215,6 @@ class _WeightPageState extends State<WeightPage> {
                       style: TextStyle(
                         color: ColorPalette.currentColorPalette.hintText,
                         fontSize: 18,
-                        fontFamily: "Rubik",
                       ),
                     ),
               
@@ -206,7 +225,6 @@ class _WeightPageState extends State<WeightPage> {
                       style: TextStyle(
                         color: ColorPalette.currentColorPalette.hintText,
                         fontSize: 14,
-                        fontFamily: "Rubik",
                       ),
                     ),
                       

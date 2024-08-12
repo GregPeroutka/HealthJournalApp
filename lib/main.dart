b69009757dea3eee48f2bfa3ec50fea99ad0712a
Alex Lyons <alyons51846@gmail.com>
2024-08-11 21:32:36 -0500

    Fit App to Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/services.dart';
 import 'package:my_health_journal/app_style.dart';
 import 'package:my_health_journal/views/screen_views/screen_controller_view.dart';
 import 'package:flutter/material.dart';
@@ -11,6 +12,19 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    
+    //Setting SysemUIOverlay
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      systemStatusBarContrastEnforced: false,
+      systemNavigationBarColor: Colors.transparent,
+      systemNavigationBarDividerColor: Colors.transparent,
+      systemNavigationBarIconBrightness: Brightness.light,
+      statusBarIconBrightness: Brightness.light)
+    );
+    
+    //Setting SystmeUIMode
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge, overlays: [SystemUiOverlay.top]);
+    
     return MaterialApp(
       theme: ThemeData(
         fontFamily: AppStyle.currentStyle.fontFamily
